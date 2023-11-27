a60c5ee7228c6136aab83a61cc748626e26c1bfb
RoyBraks <roybraks@gmail.com>
2023-11-27 14:09:31 +0100

    added column text container with animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const lines = document.querySelectorAll('.text--svg--animated-line');
-    const textHeader = document.querySelectorAll('.text--header');
+    const lines = document.querySelectorAll('.animate-line');
+    const textHeader = document.querySelectorAll('.hero-container-text--header');
     const ctaHero = document.querySelectorAll('.cta-container-explorebrainwave')
 
     const lineAnimation = gsap.timeline({ paused: true });
@@ -43,6 +43,17 @@ document.addEventListener("DOMContentLoaded", function () {
         ease: 'power2.Out'
     })
 
+    gsap.from(".main-text-container-column", {
+        scrollTrigger: {
+            trigger: ".main-text-container-column",
+            toggleActions: "restart restart restart restart"
+        },
+        y: 50,
+        opacity: 0,
+        duration: 1,
+        ease: 'power2.Out'
+    })
+
     ctaHeroAnimation.fromTo(
         ctaHero,
         {opacity: 0, y: 10 },
@@ -58,5 +69,33 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function() {
         ctaHeroAnimation.play();
     }, 600)
+
+    let mouseDown = false;
+    let startX, scrollLeft;
+    const slider = document.querySelector('#draggable-container');
+
+    const startDragging = (e) => {
+      mouseDown = true;
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    }
+
+    const stopDragging = (e) => {
+      mouseDown = false;
+    }
+
+    const move = (e) => {
+      e.preventDefault();
+      if(!mouseDown) { return; }
+      const x = e.pageX - slider.offsetLeft;
+      const scroll = x - startX;
+      slider.scrollLeft = scrollLeft - scroll;
+    }
+    
+    // Add the event listeners
+    slider.addEventListener('mousemove', move, false);
+    slider.addEventListener('mousedown', startDragging, false);
+    slider.addEventListener('mouseup', stopDragging, false);
+    slider.addEventListener('mouseleave', stopDragging, false);
 });
 
